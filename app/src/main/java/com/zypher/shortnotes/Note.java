ef3ad45e1d7cbf86d3bfddfc46ed534a3ba9b8cb
ktnguyen01 <ktnguyen01@email.wm.edu>
2024-03-08 07:39:33 -0500

    Simple android note taking app with pinning and deleting.

---
+++ app/src/main/java/com/zypher/shortnotes/Note.java
@@ -0,0 +1,41 @@
+package com.zypher.shortnotes;
+
+public class Note {
+    private String title;
+    private String content;
+
+    private boolean isPinned;
+
+    public Note() {
+
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Getter and setter for isPinned
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
